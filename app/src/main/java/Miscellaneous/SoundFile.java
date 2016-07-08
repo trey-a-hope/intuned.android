ad5ef7d1c1e6330369bc10c44b5fb7e97b5e0243
hopet1 <trey.a.hope@gmail.com>
2016-07-08 12:41:28 -0600

    Stupid Mutha Fuckin Merge

--- app/src/main/java/Miscellaneous/SoundFile.java
+++ app/src/main/java/Miscellaneous/SoundFile.java
@@ -625,7 +625,6 @@ public class SoundFile {
                 outputStream.write(buffer, 0, remaining);
             }
             outputStream.close();
-            System.out.print("Success");
         } catch (IOException e) {
             Log.e("Ringdroid", "Failed to create the .m4a file.");
             Log.e("Ringdroid", getStackTrace(e));
